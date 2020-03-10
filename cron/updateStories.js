192feeb73bb6dbdb068a73ecc1a7e04d2b117569
kidcompassion <sallypoulsen@gmail.com>
2020-03-10 10:28:34 -0600

    Adding auth to post

--- cron/updateStories.js
+++ cron/updateStories.js
@@ -4,7 +4,10 @@ const updateStories = async ()=>{
     const url = 'https://headlines-fe.herokuapp.com/api/stories'
 
     try{
-        await axios.post( url ).then(()=>{
+        await axios.post( url, {
+            "emailAddress": "sallypoulsen@gmail.com",
+            "password": $2b$10$hekFJ7Cj58xpJr4k89uc5uEGUNE6tNXcAzqfrr6qtKhNDUozdNq6W
+        }).then(()=>{
             console.log('posted');
         });
     }catch(err){

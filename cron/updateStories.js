c5f0443e21e4c4fd8ee310530797fd5def92f2e5
kidcompassion <sallypoulsen@gmail.com>
2020-03-10 10:09:20 -0600

    Adds try catch.

--- cron/updateStories.js
+++ cron/updateStories.js
@@ -2,9 +2,15 @@ const axios = require('axios');
 
 const updateStories = async ()=>{
     const url = 'https://headlines-fe.herokuapp.com/api/stories'
-    await axios.post( url, { method: 'POST'}).then(()=>{
-        console.log('posted');
-      });
+
+    try{
+        await axios.post( url, { method: 'POST'}).then(()=>{
+            console.log('posted');
+        });
+    }catch(err){
+        console.log(err);
+    }
+    
 }
 
 updateStories();

0b9394803585d15cda16e82514b6a15548b31ad7
kidcompassion <sallypoulsen@gmail.com>
2020-03-10 10:30:07 -0600

    Forgot to make the password a string.

--- cron/updateStories.js
+++ cron/updateStories.js
@@ -6,7 +6,7 @@ const updateStories = async ()=>{
     try{
         await axios.post( url, {
             "emailAddress": "sallypoulsen@gmail.com",
-            "password": $2b$10$hekFJ7Cj58xpJr4k89uc5uEGUNE6tNXcAzqfrr6qtKhNDUozdNq6W
+            "password": "$2b$10$hekFJ7Cj58xpJr4k89uc5uEGUNE6tNXcAzqfrr6qtKhNDUozdNq6W"
         }).then(()=>{
             console.log('posted');
         });

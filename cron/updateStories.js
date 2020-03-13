5e67d84c5dfac568370c89722d5630a8c00d9db2
kidcompassion <sallypoulsen@gmail.com>
2020-03-13 14:04:08 -0600

    Updates cron job to try to get it working.

--- cron/updateStories.js
+++ cron/updateStories.js
@@ -1,10 +1,17 @@
 const axios = require('axios');
 
+const user = {
+    "emailAddress": "sallypoulsen@gmail.com",
+    "password": "Bikinikill_66"
+}
+
+const encodedCredentials = btoa(`${user.emailAddress}:${user.password}`);
+
 const updateStories = async ()=>{
     const url = 'https://headlines-fe.herokuapp.com/api/stories'
-
+    axios.post(url, { user, headers: {"Authorization" : `Basic ${encodedCredentials}`} });
     try{
-        await axios.post( url).then(()=>{
+        await axios.post( url, ).then(()=>{
             console.log('posted');
         });
     }catch(err){

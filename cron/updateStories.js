9afbe4cff94f84ca78b4b5275df4622ae6c6bd61
kidcompassion <sallypoulsen@gmail.com>
2020-03-13 15:24:12 -0600

    Trying to debug the cron job.

--- cron/updateStories.js
+++ cron/updateStories.js
@@ -2,17 +2,10 @@ const axios = require('axios');
 
 const updateStories = async ()=>{
 
-    let axiosConfig = {
-        headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*",
-        }
-      };
-
     const url = 'https://headlines-fe.herokuapp.com/api/stories'
 
     try{
-        await axios.post( url, {},axiosConfig).then(()=>{
+        await axios.post( url).then(()=>{
             console.log('posted');
         });
     }catch(err){
